ae9cafcf0019cef596c588398c9c9887603a2d4f
Daniel Blanco <64988654+DnlBM@users.noreply.github.com>
2023-05-07 14:24:03 -0500

    Completed Getting Started Part 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
@@ -11,9 +11,12 @@ const BlogPage = ({ data }) => {
       {
         data.allMdx.nodes.map((node) => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <p>{node.excerpt}</p>
           </article>
         ))
       }
@@ -26,11 +29,11 @@ export const query = graphql`
     allMdx(sort: { frontmatter: { date: DESC }}) {
       nodes {
         frontmatter {
-          title
           date(formatString: "MMMM DD, YYYY")
+          title
+          slug
         }
         id
-        excerpt
       }
     }
   }
